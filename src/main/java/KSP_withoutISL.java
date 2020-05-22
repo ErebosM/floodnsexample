bb7fb4579bcd7d4d774196980c58dc6f5d419527
manuelgr <manuelgr@ethz.ch>
2020-05-22 16:22:13 +0200

    simplified KSP_withoutISL

--- src/main/java/KSP_withoutISL.java
+++ src/main/java/KSP_withoutISL.java
@@ -15,39 +15,37 @@ public class KSP_withoutISL {
     public static final int KSP_K = 1;
 
     public static void main(String[] args) {
-        for (int i = 20; i <= 20; i += 5) {
-            String folderPath = "/topo_" + i + "Tbit_" + DURATION + "s_withoutISL";
+        String folderPath = "/topo_" + 20 + "Tbit_" + DURATION + "s_withoutISL";
 
-            // Random number generators
-            Random routingRandom = new Random(4839252);
+        // Random number generators
+        Random routingRandom = new Random(4839252);
 
-            // Fat-tree topology
-            Topology topology = FileToTopologyConverter.convert(
-                    "/home/manuelgr/OneDrive/Master Thesis/Simulators/FloodNS/topologies" + folderPath
-                            + "/satellite_constellation.properties",
-                    20,
-                    20, // 100 flow units / time unit ("bit/ns")
-                    true); // 20 flow units / time unit ("bit/ns");
-            Network network = topology.getNetwork();
+        // Fat-tree topology
+        Topology topology = FileToTopologyConverter.convert(
+                "/home/manuelgr/OneDrive/Master Thesis/Simulators/FloodNS/topologies" + folderPath
+                        + "/satellite_constellation.properties",
+                20, // 20 flow units / time unit ("bit/ns")
+                20, // 20 flow units / time unit ("bit/ns")
+                true);
+        Network network = topology.getNetwork();
 
-            // Create simulator
-            Simulator simulator = new Simulator();
-            FileLoggerFactory loggerFactory = new FileLoggerFactory(simulator, "temp" + folderPath);
-            Aftermath aftermath = new SimpleMmfAllocator(simulator, network);
-            simulator.setup(network, aftermath, loggerFactory);
+        // Create simulator
+        Simulator simulator = new Simulator();
+        FileLoggerFactory loggerFactory = new FileLoggerFactory(simulator, "temp" + folderPath);
+        Aftermath aftermath = new SimpleMmfAllocator(simulator, network);
+        simulator.setup(network, aftermath, loggerFactory);
 
-            // Routing
-            KspRoutingStrategy routingStrategy = new KspRoutingStrategy(simulator, topology, routingRandom, KSP_K);
+        // Routing
+        KspRoutingStrategy routingStrategy = new KspRoutingStrategy(simulator, topology, routingRandom, KSP_K);
 
-            // Traffic
-            Schedule schedule = new Schedule("/home/manuelgr/OneDrive/Master Thesis/Simulators/FloodNS/topologies"
-                    + folderPath + "/trafficSchedule.properties", topology, (long) DURATION * (long) 1e9);
-            simulator.insertEvents(schedule.getConnectionStartEvents(simulator, routingStrategy));
+        // Traffic
+        Schedule schedule = new Schedule("/home/manuelgr/OneDrive/Master Thesis/Simulators/FloodNS/topologies"
+                + folderPath + "/trafficSchedule.properties", topology, (long) DURATION * (long) 1e9);
+        simulator.insertEvents(schedule.getConnectionStartEvents(simulator, routingStrategy));
 
-            // Run the simulator
-            simulator.run((long) DURATION * (long) 1e9); // 5e9 time units ("ns")
-            loggerFactory.runCommandOnLogFolder("python external/analyze.py");
-        }
+        // Run the simulator
+        simulator.run((long) DURATION * (long) 1e9); // 5e9 time units ("ns")
+        loggerFactory.runCommandOnLogFolder("python external/analyze.py");
 
     }
 
