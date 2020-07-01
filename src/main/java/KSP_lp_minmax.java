28ea3c6b7787468910f43c1df97c23e21ff8a6c1
manuelgr <manuelgr@ethz.ch>
2020-07-01 16:36:37 +0200

    added KSP_lp_minmax.java

---
+++ src/main/java/KSP_lp_minmax.java
@@ -0,0 +1,58 @@
+import ch.ethz.systems.floodns.core.Aftermath;
+import ch.ethz.systems.floodns.core.Network;
+import ch.ethz.systems.floodns.core.Simulator;
+import ch.ethz.systems.floodns.ext.allocator.MinMaxLinkCapLpAllocator;
+import ch.ethz.systems.floodns.ext.basicsim.topology.FileToTopologyConverter;
+import ch.ethz.systems.floodns.ext.basicsim.topology.Topology;
+import ch.ethz.systems.floodns.ext.logger.file.FileLoggerFactory;
+import ch.ethz.systems.floodns.ext.basicsim.schedule.Schedule;
+import ch.ethz.systems.floodns.ext.lputils.GlopLpSolver;
+import ch.ethz.systems.floodns.ext.routing.KspMultiPathRoutingStrategy;
+
+public class KSP_lp_minmax {
+        public static void main(String[] args) {
+
+                final int DURATION = Integer.parseInt(args[0]);
+                final String RESULTS_FOLDER_NAME = args[1];
+                final String FOLDER_NAME = args[2];
+                final int KSP_K = Integer.parseInt(args[3]);
+                final int UPDOWN_CAPACITY = Integer.parseInt(args[4]);
+                final int ISL_CAPACITY = Integer.parseInt(args[5]);
+                final int UPPER_SAT_ID = Integer.parseInt(args[6]);
+                final int NUM_CITIES = Integer.parseInt(args[7]);
+                final String PATH_FILE = args[8];
+                final String ROUTING_TYPE = args[9];
+
+                String folderPath = "/home/manuelgr/master_thesis/Simulators/FloodNS/" + RESULTS_FOLDER_NAME + "/"
+                                + FOLDER_NAME;
+                String path_file = "/home/manuelgr/master_thesis/Simulators/FloodNS/" + PATH_FILE;
+
+                Topology topology = FileToTopologyConverter.convert(
+                                folderPath + "/topo/satellite_constellation.properties", UPDOWN_CAPACITY, ISL_CAPACITY,
+                                true, UPPER_SAT_ID);
+                Network network = topology.getNetwork();
+
+                // Create simulator
+                Simulator simulator = new Simulator(1e-9);
+                FileLoggerFactory loggerFactory = new FileLoggerFactory(simulator, folderPath);
+                Aftermath aftermath = new MinMaxLinkCapLpAllocator(simulator, network, null,
+                                new GlopLpSolver("/home/manuelgr/floodnsexample/external/glop_solver.py"));
+                simulator.setup(network, aftermath, loggerFactory);
+
+                // Routing
+                KspMultiPathRoutingStrategy routingStrategy = new KspMultiPathRoutingStrategy(simulator, topology,
+                                KSP_K, UPPER_SAT_ID, NUM_CITIES, path_file, ROUTING_TYPE);
+
+                // Traffic
+                Schedule schedule = new Schedule(folderPath + "/topo/trafficSchedule.properties", topology,
+                                (long) DURATION * (long) 1e9);
+                simulator.insertEvents(schedule.getConnectionStartEvents(simulator, routingStrategy));
+
+                // Run the simulator
+                simulator.run((long) DURATION * (long) 1e9); // 5e9 time units ("ns")
+
+                loggerFactory.runCommandOnLogFolder("python3 /home/manuelgr/floodnsexample/external/analyze.py",
+                                UPPER_SAT_ID + " " + NUM_CITIES);
+        }
+
+}
